2493f9cd09eecbba8260b16fe6b7344a80d5f569
thomas-waite <thomas.waite4@gmail.com>
2022-11-10 23:12:45 -0800

    refactor: update for scroll

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# zkEVM comparison
+# Deploy a test ER20 to Scroll zkEVM
+This demo deploys a simple ERC20Mintable to the Scroll zkEVM Alpha testnet, and then mints and transfers some tokens. It uses the Hardhat development framework.
+
+## 1. Getting started
+1. `npm install`
+2. Copy `.env.example` to `.env` and populate `PRIVATE_KEY` with your Ethereum private key. Leave the `SCROLL_TESTNET_URL`
+
+## 2. Deploy to Scroll, mint and transfer tokens
+There is an `ERC20Mintable.sol` in `contracts/`. This is a simple ERC20 with an unprotected `mint()` function - do not use it in production. We will deploy this contract to Scroll. Following that we will mint 
+
+
+1. Compile the contract, run `npm run compile`
 Deploys a simple ERC20 smart contract to various zkEVM's to explore the capabilities of different platforms. 
 
-Started with Scroll and zkSync
+## 3. Minting and transferring tokens
---
+++ deploy/index.ts
@@ -0,0 +1,38 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const deployerSigner = (await ethers.getSigners())[0];
+  const deployerAddress = deployerSigner.address;
+
+  // 1. Deploy ERC20
+  const ERC20Mintable = await ethers.getContractFactory("ERC20Mintable");
+  const token = await ERC20Mintable.deploy();
+  await token.deployed();
+  console.log('Token deployed at: ', token.address);
+
+  const erc20MintableABI = [
+    'function mint(address,uint256) returns (bool)',
+    'function transfer(address,uint256) returns (bool)',
+    'function balanceOf(address) view returns (uint256)'
+  ];
+
+  // 2. Mint 1000 tokens to user
+  const mintTx = await token.mint(deployerAddress, 1000);
+  console.log('Pending mint tx hash: ', mintTx.hash);
+  await mintTx.wait();
+  console.log('Mint processed');
+
+  // 3. Transfer 10 tokens to a second user
+  const receiver = '0x803554C9cB72227D88B56495D3E92f96aD589B09';
+  const transferTx = await token.transfer(receiver, 10);
+  console.log('Pending transfer tx hash: ', transferTx.hash)
+  await transferTx.wait();
+  console.log('Transfer processed')
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
